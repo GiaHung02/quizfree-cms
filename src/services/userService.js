9e8cda826d7e1550c4bc87778e613dc0c3b66a04
banhbaoratlangon <phamgiahung02@gmail.com>
2025-12-11 22:28:11 +0700

    remove console.log

--- src/services/userService.js
+++ src/services/userService.js
@@ -4,14 +4,9 @@ import Cookies from 'js-cookie';
 
 // LOGIN USER
 export async function login(emailOrUsername, password) {
-
-    console.log(emailOrUsername, password);
-    console.log("11111");
-    
     if (!emailOrUsername || !password) {
         return { success: false, message: 'username and password are required' };
     }
-    console.log("222222");
     
     const res = await fetch(`${API_URL}/user/login`, {
         method: 'POST',
@@ -22,13 +17,7 @@ export async function login(emailOrUsername, password) {
     });
     if(!res.ok) throw new Error('Failed to login, please check your username and password');
     
-    console.log("33333");
     const data = await res.json();
-    console.log("data: ", data);
-
-    
     Cookies.set("user", JSON.stringify(data.data), { expires: 7 });
-    console.log("4444");
-    
     return data;
 }
